e0b91d12ebd4be9655f4e9195b5bc998ab5ce0b4
Carlos Augusto <augustino2003@hotmail.com>
2021-06-28 12:13:40 -0300

    Desafio TDD - 6 Tests checks

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,43 @@
+package com.devsuperior.bds02.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class CityService {
+	
+	@Autowired
+	private CityRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<CityDTO> findAll(){
+		List<City> lista = repository.findAll(Sort.by("name"));
+		return lista.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+
+	public void delete(Long id) throws Exception {
+		try {
+			repository.deleteById(id);
+			
+		}catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id " + id + " not found");
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+		
+	}
+
+}
